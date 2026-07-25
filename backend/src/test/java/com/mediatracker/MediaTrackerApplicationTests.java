0696241efc42f08ab1ccd589e9afcd3d0aa49538
Shrutika Londhe <shrutikalondhe15@gmail.com>
2026-07-25 21:35:31 +0530

    Backend running successfully against local MySQL

--- backend/src/test/java/com/mediatracker/MediaTrackerApplicationTests.java
+++ backend/src/test/java/com/mediatracker/MediaTrackerApplicationTests.java
@@ -2,7 +2,7 @@ package com.mediatracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
